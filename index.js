925e2b399c3d565ae6a9adcf3c96b6b9d4935dd7
George Lima <georgelima11@hotmail.com>
2020-04-12 13:58:30 -0300

    chore: wait service update before test

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const core = require('@actions/core')
 const child_process = require('child_process')
 
-try {
-  const entrypoint = core.getInput('entrypoint')
-  const asSu = core.getInput('run-as-su')
-  const serviceName = core.getInput('service-name')
-
-  const updateStatus = child_process
+const getStatus = ({ entrypoint, asSu, serviceName }) =>
+  child_process
     .execSync(
       `${entrypoint || ''} ${
         asSu ? 'sudo' : ''
@@ -15,11 +11,47 @@ try {
     .toString()
     .trim()
 
-  if (updateStatus !== 'completed') {
-    return core.setFailed(`${serviceName} update status is: ${updateStatus}, please verify your deployment!`)
-  }
+const awaitUpdate = ({ entrypoint, asSu, serviceName }) =>
+  new Promise((resolve, reject) => {
+    let interval
 
-  console.log('All good!')
-} catch (err) {
-  core.setFailed(err.message)
+    try {
+      interval = setInterval(() => {
+        const updateStatus = getStatus({ entrypoint, asSu, serviceName })
+        if (updateStatus === 'updating') {
+          console.log(`Service status: ${updateStatus}`)
+          return
+        }
+
+        clearInterval(interval)
+        resolve()
+      }, 3000)
+    } catch (err) {
+      if (interval) clearInterval(interval)
+      reject(err)
+    }
+  })
+
+async function run() {
+  try {
+    const entrypoint = core.getInput('entrypoint')
+    const asSu = core.getInput('run-as-su')
+    const serviceName = core.getInput('service-name')
+
+    console.log('Waiting service update...')
+
+    await awaitUpdate({ entrypoint, asSu, serviceName })
+
+    const updateStatus = getStatus({ entrypoint, asSu, serviceName })
+
+    if (updateStatus !== 'completed') {
+      return core.setFailed(`${serviceName} update status is: ${updateStatus}, please verify your deployment!`)
+    }
+
+    console.log('All good!')
+  } catch (err) {
+    core.setFailed(err.message)
+  }
 }
+
+run()
